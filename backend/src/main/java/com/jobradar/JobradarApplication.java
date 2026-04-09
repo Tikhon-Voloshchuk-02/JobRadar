ba3d89a3e5005940c57542a437ca2f68be6a34b5
Tikhon <voloshchuk@uni-potsdam.de>
2026-04-10 00:17:12 +0200

    initial project structure

---
+++ backend/src/main/java/com/jobradar/JobradarApplication.java
@@ -0,0 +1,13 @@
+package com.jobradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobradarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jobradar/JobradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
