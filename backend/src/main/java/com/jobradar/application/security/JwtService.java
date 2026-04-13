b9fe0ddddd17f26b359ac5d32866414e35df5607
Tikhon <voloshchuk@uni-potsdam.de>
2026-04-13 03:23:48 +0200

    add authentication (login & JWT) with tests

---
+++ backend/src/main/java/com/jobradar/application/security/JwtService.java
@@ -0,0 +1,35 @@
+package com.jobradar.application.security;
+
+import com.jobradar.application.model.user.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    private SecretKey getSignInKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
--- backend/src/main/java/com/jobradar/application/service/AuthService.java
+++ backend/src/main/java/com/jobradar/application/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.jobradar.application.service;
 
+import com.jobradar.application.dto.AuthResponse;
+import com.jobradar.application.dto.LoginRequest;
 import com.jobradar.application.dto.RegisterRequest;
 import com.jobradar.application.exception.EmailAlreadyExistsException;
 import com.jobradar.application.model.user.Role;
 import com.jobradar.application.model.user.User;
-import com.jobradar.application.model.user.UserRepository;
+import com.jobradar.application.repository.UserRepository;
+import com.jobradar.application.security.JwtService;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -12,9 +17,17 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
-    public AuthService(UserRepository userRepository){
-        this.userRepository=userRepository;
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       JwtService jwtService) {
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+
     }
 
     public User register(RegisterRequest request){
@@ -26,10 +39,27 @@ public class AuthService {
         user.setFirstname(request.getFirstname());
         user.setLastname(request.getLastname());
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(Role.USER); //USER-role auto
 
         return userRepository.save(user);
 
+    }
+
+    public AuthResponse login(LoginRequest request){
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new BadCredentialsException("Invalid email or password"));
+
+        boolean passwordMatches = passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword()
+        );
+
+        if(!passwordMatches){
+            throw new BadCredentialsException("Invalid Email/Password");
+        }
+
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
     }
 }
